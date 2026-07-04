3bf9121c53f1b154dc3fea5189bcf446ea40667a
lisettediazsantiago-commits <lisettediazsantiago@gmail.com>
2026-07-03 20:57:17 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ComfortMap from "./screens/ComfortMap";
 import Journal from "./screens/Journal";
 import Snapshot from "./screens/Snapshot";
 import Timeline from "./screens/Timeline";
+import Reflect from "./screens/Reflect";
 import Intimacy from "./screens/Intimacy";
 import SharedSpace from "./screens/SharedSpace";
 import CheckIn from "./screens/CheckIn";
@@ -42,6 +43,7 @@ export default function App() {
               <Route path="journal" element={<Journal />} />
               <Route path="snapshot" element={<Snapshot />} />
               <Route path="timeline" element={<Timeline />} />
+              <Route path="reflect" element={<Reflect />} />
               <Route path="settings" element={<Settings />} />
             </Route>
 
